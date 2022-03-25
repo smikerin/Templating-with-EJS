2944f9316e1e5f8a5934696102a761e1febed889
Sergey Mikerin <91702021+smikerin@users.noreply.github.com>
2022-03-25 06:49:06 +0000

    finished users

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
- 
+
+//here's our test data 
+var data = require('./data/test.json');
+
+
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -10,6 +14,24 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
   res.render('pages/index',{'title':title});
+});
+
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('Users/view', {
+     title: title,
+     user: data[--id]
+ });
 });
 
 app.get('/190E', (req, res) => {
